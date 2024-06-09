3c267142140a7cdabb98f01a77e65dfe3ee48a06
Eric2787 <ericg7346@gmail.com>
2024-06-09 02:46:54 -0600

    IND_MAKAK v1.0.05

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,6 +51,7 @@ dependencies {
     implementation(libs.androidx.legacy.support.v4)
     implementation(libs.androidx.fragment.ktx)
     implementation(libs.androidx.library)
+    implementation(libs.androidx.annotation)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/com/example/ind_makak/ui/gallery/GalleryFragment.kt
+++ app/src/main/java/com/example/ind_makak/ui/gallery/GalleryFragment.kt
@@ -5,6 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import com.example.ind_makak.R
 import com.example.ind_makak.databinding.FragmentGalleryBinding
 
 class GalleryFragment : Fragment() {
@@ -19,6 +21,9 @@ class GalleryFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentGalleryBinding.inflate(inflater, container, false)
+        binding.button13.setOnClickListener {
+            view: View -> view.findNavController().navigate(R.id.action_fragment_gallery_to_fragment_home)
+        }
         return binding.root
     }
 
